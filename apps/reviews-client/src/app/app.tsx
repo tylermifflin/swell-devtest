244bcf33e16fdf898ddbc24a9c0bf3bf27dfa0e8
Eric Gibby <eric@swellcx.com>
2022-08-31 12:25:44 -0600

    Stubbed out reviews list component

--- apps/reviews-client/src/app/app.tsx
+++ apps/reviews-client/src/app/app.tsx
@@ -1,6 +1,8 @@
+import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import WebFont from 'webfontloader';
 import Header from './components/header/header';
+import ReviewsList from './components/reviews-list/reviews-list';
 import { theme } from './theme';
 
 WebFont.load({
@@ -13,6 +15,9 @@ export function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<Header />
+			<Container sx={{ mt: 2, typography: 'body1' }}>
+				<ReviewsList />
+			</Container>
 		</ThemeProvider>
 	);
 }
---
+++ apps/reviews-client/src/app/components/reviews-list/reviews-list.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable-next-line */
+export interface ReviewsListProps {}
+
+export function ReviewsList(props: ReviewsListProps) {
+	return <>TODO: Implement ReviewsList</>;
+}
+
+export default ReviewsList;
