3e8453e1e33191fdc8330e9a0f0912607eb59356
Eric Gibby <eric@swellcx.com>
2022-09-01 14:16:11 -0600

    Stubbed out tests for reviews list component

--- apps/reviews-client/src/app/components/reviews-list/reviews-list.spec.tsx
+++ apps/reviews-client/src/app/components/reviews-list/reviews-list.spec.tsx
@@ -6,4 +6,18 @@ describe('ReviewsList', () => {
 		const { baseElement } = render(<ReviewsList />);
 		expect(baseElement).toBeTruthy();
 	});
+
+	it.todo('should render list of reviews');
+
+	it.todo('should display message if no reviews are found');
+
+	it.todo('should display the date of the review in a human readable format');
+
+	it.todo('should display the name of the reviewer');
+
+	it.todo('should display the name of the company');
+
+	it.todo('should display the review text if provided');
+
+	it.todo('should display the rating if provided');
 });
