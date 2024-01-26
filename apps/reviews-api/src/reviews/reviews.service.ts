36bc6b94c216e03c2d4d9dfa89f9e0798d670af5
tylermifflin <tyler.mifflin1@hotmail.com>
2024-01-26 15:21:40 -0700

    added getReviews into ReviewService class so that it can be used in the controller

--- apps/reviews-api/src/reviews/reviews.service.ts
+++ apps/reviews-api/src/reviews/reviews.service.ts
@@ -5,6 +5,10 @@ import { DatabaseService } from '../database/database.service';
 export class ReviewsService {
 	constructor(private prisma: DatabaseService) {}
 
+	async getReviews() {
+		return this.prisma.review.findMany();
+	}
+
 	getReviewsCount() {
 		return this.prisma.review.count();
 	}
