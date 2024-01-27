d15ad09eb3c3c4b36e8310814be3819afb9d884c
tylermifflin <tyler.mifflin1@hotmail.com>
2024-01-26 17:04:35 -0700

    set up the interface for Review to include the id, reviewer name, company name, rating, text, and createdOn

--- apps/reviews-client/src/app/components/reviews-list/reviews-list.tsx
+++ apps/reviews-client/src/app/components/reviews-list/reviews-list.tsx
@@ -5,6 +5,19 @@ import React, { useState, useEffect } from 'react';
 /* eslint-disable-next-line */
 export interface ReviewsListProps {}
 
+interface Review {
+	id: string;
+	reviewer: {
+		name: string;
+	};
+	company: {
+		name: string;
+	};
+	rating?: number;
+	text?: string;
+	createdOn: string;
+}
+
 export function ReviewsList(props: ReviewsListProps) {
 	return (
 		<Alert severity="error" icon={<TaskIcon />}>
